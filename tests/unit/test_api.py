a8c0b3a326f2bfe39ea593af53dc524e429bc78a
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-03-04 21:05:38 +0700

    Add test_create_instance_with_wrong_locale

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,3 +1,4 @@
+from pyleo.exceptions.arg_error import LocaleError
 from pyleo.api import LeoApi
 from urllib import parse
 import unittest
@@ -25,3 +26,7 @@ class TestApi(unittest.TestCase):
         expected = 'https://lingualeo.com/userdict3/getTranslations'
         result = self._test_instance.construct_url(query_string, word)
         self.assertEqual(expected, result)
+
+    def test_create_instance_with_wrong_locale(self):
+        with self.assertRaises(LocaleError) as e:
+            LeoApi(TestApi.fake_email, TestApi.fake_password, 'ro')
